68e1e0b2a7308838c9d81d7e79e4e9a747fe504b
Surenderkohli77 <surenderkohli77@gmail.com>
2024-11-12 20:18:28 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import SearchBar from "./components/searchBar";
+import CanvasEditor from "./components/canvasEditor";
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddCaptionPage from "./AddCaptionPage";
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  // Function to handle image selection
+  const onImageSelect = (image) => {
+    setSelectedImage(image); // Updates the state with the selected image
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  //   <div className="">
+  //   <h1 className="text-3xl font-bold underline">Image Editor</h1>
+  //   <SearchBar onImageSelect={onImageSelect} />{" "}
+  //   {/* Pass the function as a prop */}
+  //   {selectedImage && <CanvasEditor image={selectedImage} />}{" "}
+  //   {/* Pass selected image */}
+  // </div>
+
+
+<Router>
+      <Routes>
+        
+        <Route path="/" element={<SearchBar onImageSelect={setSelectedImage} />} />
+        <Route path="/add-caption" element={<AddCaptionPage imageUrl={selectedImage} />} />
+      </Routes>
+    </Router>
   );
 }
 
